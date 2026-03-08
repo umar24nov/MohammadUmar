3d6108fc15e7ad50821dea20219722f99dfd16da
umar24nov <umar24nov@gmail.com>
2026-03-08 06:04:31 +0530

    feat: firebase feedback form

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,24 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC-LKgOnc7EE7VbW9L82cqrNtMexMO61zM",
+  authDomain: "portfolio-8239d.firebaseapp.com",
+  projectId: "portfolio-8239d",
+  storageBucket: "portfolio-8239d.firebasestorage.app",
+  messagingSenderId: "222528097203",
+  appId: "1:222528097203:web:07c5f9ce22cf580df819c6",
+  measurementId: "G-CW1WYPWYSP"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
